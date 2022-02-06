42d4b7aa1d705d9fc0250dba09a1d924558bdc4c
KR <kyranrawlinson@gmail.com>
2022-02-06 19:02:50 +0000

    UPDATED tests/test_fund_me.py
    Added test_only_owner_can_withdraw test, and updated for expected error on ganache-local-persistant network

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,3 +1,6 @@
+import pytest
+from brownie import accounts, config, network, exceptions
+
 from scripts.general_scripts import get_account, deploy_mocks
 from scripts.deploy import deploy_fund_me
 
@@ -15,4 +18,11 @@ def test_can_fund_and_withdraw():
     tx2.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0
     
-    
+def test_only_owner_can_withdraw():
+    if config['networks'][network.show_active()]['local'] is False:
+        pytest.skip('Only tested on local networkss')
+
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    with pytest.raises(ValueError):
+        fund_me.withdraw({"from": bad_actor})
