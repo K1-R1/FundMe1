62c287da5bda8c0a15a83f96207713fd204382d1
KR <kyranrawlinson@gmail.com>
2022-02-06 17:18:19 +0000

    UPDATED general_scripts.py
    Updated with parameterised variable, and to use config-network-local

--- scripts/general_scripts.py
+++ scripts/general_scripts.py
@@ -1,16 +1,16 @@
 from brownie import MockV3Aggregator, network, accounts, config
 
-decimals = 8
-starting_value = 2500
+DECIMALS = 8
+STARTING_VALUE = 2500
 
 def get_account():
-    if network.show_active() == 'development':
+    if config['networks'][network.show_active()]['local']:
         return accounts[0]
     else:
         return accounts.add(config['wallets']['dev_account_1']['private_key'])
 
 def deploy_mocks():
     if not len(MockV3Aggregator):
-        MockV3Aggregator.deploy(decimals, starting_value * 10 ** 8, {'from': get_account()})
+        MockV3Aggregator.deploy(DECIMALS, STARTING_VALUE * 10 ** 8, {'from': get_account()})
     print('Mocks deployed\n')
     
