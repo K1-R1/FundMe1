160593be99370eced0da9d8f03431cb1396c9ebb
KR <kyranrawlinson@gmail.com>
2022-02-06 16:31:03 +0000

    UPDATED deploy.py
    Updated to be network agnostic; check current active network, if development deploy mocks if necessary and dont verify source, if live set price feed contract and verify source

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,17 @@
-from brownie import FundMe
-from .general_scripts import get_account
+from brownie import FundMe, MockV3Aggregator, network, config
+from .general_scripts import get_account, deploy_mocks
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({'from': account}, publish_source=True)
+    _network = network.show_active()
+    print(f"Active network: {_network}")
+    if _network != 'development':
+        eth_usd_price_feed = config['networks'][_network]['eth_usd_price_feed']
+    else:
+        deploy_mocks()
+        eth_usd_price_feed = MockV3Aggregator[-1].address
+    FundMe.deploy(eth_usd_price_feed, {'from': account}, 
+                  publish_source=config['networks'][_network]['verify'])
 
 
 def main():
